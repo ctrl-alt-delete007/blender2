9201283770eef9ae0b8f8861026e0849593e2640
cfernando415 <ca.fernando415@gmail.com>
2019-04-21 11:55:40 -0400

    added how long the post since created

--- client/src/component/Card.js
+++ client/src/component/Card.js
@@ -1,6 +1,18 @@
 import React from "react";
 
 const Card = props => {
+  const oneDay = 24 * 60 * 60 * 1000;
+  const dateNow = new Date();
+  const datePosted = new Date(props.post.created_at);
+  const daysAgo = Math.round(
+    Math.abs((dateNow.getTime() - datePosted.getTime()) / oneDay)
+  );
+  const postedDaysAgo =
+    daysAgo === 0
+      ? "< 1 day ago"
+      : daysAgo === 1
+      ? "1 day ago"
+      : `${daysAgo} days ago`;
   return (
     <div style={{ marginRight: "10px" }} className="wrapper">
       <img
@@ -9,6 +21,18 @@ const Card = props => {
         alt={props.post.screen_name}
       />
       <div className="overlay ctr imghov">
+        <span>
+          <img
+            className="ui avatar image"
+            src={props.post.profile_image_url}
+            alt={props.post.screen_name}
+          />
+          <span style={{ color: "white" }}>
+            {props.post.screen_name}
+            <br />
+            {postedDaysAgo}
+          </span>
+        </span>
         <button>Blue</button>
       </div>
     </div>
