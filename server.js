5533968008328dd0663cdf18eae69a8c4e5594a2
cfernando415 <ca.fernando415@gmail.com>
2019-04-22 02:13:50 -0400

    pushing to heroku

--- server.js
+++ server.js
@@ -4,15 +4,14 @@ const db = require("./queries");
 
 const app = express();
 
-const port = 5000;
+const port = process.env.port || 5000;
 
 app.use(express.json());
 
-app.get("https://blenderapp.herokuapp.com:5000/api/events", db.getEvents);
+app.get("https://blenderapp.herokuapp.com/api/events", db.getEvents);
 
-app.post("https://blenderapp.herokuapp.com:5000/api/events", db.createEvent);
+app.post("https://blenderapp.herokuapp.com/api/events", db.createEvent);
 
-// need to revise the below route to GET
-app.post("/api/hashtag", twitter.getTweets);
+app.post("https://blenderapp.herokuapp.com/api/hashtag", twitter.getTweets);
 
 app.listen(port, () => console.log(`Server started on port: ${port}`));
