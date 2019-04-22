fd059f86e7085258b2eb0320d31e7b7d3dad2530
cfernando415 <ca.fernando415@gmail.com>
2019-04-22 02:22:03 -0400

    pushing to heroku

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ const port = process.env.port || 5000;
 
 app.use(express.json());
 
-app.get("https://blenderapp.herokuapp.com/api/events", db.getEvents);
+app.get(`https://blenderapp.herokuapp.com:${prot}/api/events`, db.getEvents);
 
-app.post("https://blenderapp.herokuapp.com/api/events", db.createEvent);
+app.post(`https://blenderapp.herokuapp.com:${port}/api/events`, db.createEvent);
 
-app.post("https://blenderapp.herokuapp.com/api/hashtag", twitter.getTweets);
+app.post(
+  `https://blenderapp.herokuapp.com:${port}/api/hashtag`,
+  twitter.getTweets
+);
 
 app.listen(port, () => console.log(`Server started on port: ${port}`));
