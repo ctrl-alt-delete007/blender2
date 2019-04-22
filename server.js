eec8861cf9ee6d86e5e79ce6321a4817633f27cb
cfernando415 <ca.fernando415@gmail.com>
2019-04-22 11:55:11 -0400

    pushing to digitalocean

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const port = 5000;
 
 app.use(express.json());
 
-app.get("/api/events", db.getEvents);
+app.get("http://134.209.79.135:5000/api/events", db.getEvents);
 
-app.post("/api/events", db.createEvent);
+app.post("http://134.209.79.135:5000/api/events", db.createEvent);
 
-app.post("/api/hashtag", twitter.getTweets);
+app.post("http://134.209.79.135:5000/api/hashtag", twitter.getTweets);
 
 app.listen(port, () => console.log(`Server started on port: ${port}`));
